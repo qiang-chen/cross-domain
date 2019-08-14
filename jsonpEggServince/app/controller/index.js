b365f164cb19d2a16fec63b2d165e5c2ae89a50f
qiang-chen <1715584439@qq.com>
2019-08-14 20:26:32 +0800

    手动封装ajax实现express框架的跨域处理

--- jsonpEggServince/app/controller/index.js
+++ jsonpEggServince/app/controller/index.js
@@ -2,9 +2,14 @@ const {Controller}=require("egg");
 
 class Index extends Controller{
      api(ctx){
+         const query=ctx.request.query;
+         //设置跨域处理
+         //Access-Control-Allow-Origin
+         
         ctx.body={
             code:11,
-            type:"egg返回的jsonp请求"
+            type:"egg返回的jsonp请求",
+            query
         }
     }
 }
--- jsonpExpressServince/routes/index.js
+++ jsonpExpressServince/routes/index.js
@@ -4,7 +4,7 @@ const url = require("url")
 
 var router = express.Router();
 
-
+const bodyParser=require("body-parser")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,6 +15,7 @@ router.get('/', function (req, res, next) {
 
 
 router.get("/api", (req, res, next) => {
+  
   //将script标签的src的URL请求转成对象
   const opj = url.parse(req.url, true).query;
   //然后原理就是调用这个回调函数来进行传参
@@ -23,16 +24,38 @@ router.get("/api", (req, res, next) => {
   } = opj;
   //如果这个回调函数存在证明是jsonp请求
   if (callback) {
+    //只能和end返回搭配使用  send不可以
     // res.writeHead(200, {
-      //只能和end返回搭配使用  send不可以
     //   'Content-Type': 'text/javascript'
     // });
     let resault = JSON.stringify({
       code: 1,
-      msg: "express框架传回去的参数"
+      msg: "express框架传回去jsonp的参数",
+      query:req.query
     });
-    res.end(`${callback}(${resault})`)
+    res.send(`${callback}(${resault})`)
+  }else{
+    res.writeHead(200, {
+      'Access-Control-Allow-Origin': '*'
+    })
+    res.end(JSON.stringify({
+      code:1,
+      msg: "express框架传回去egg的参数",
+      query:req.query
+    }))
   }
+})
+
+
+router.post("/apipost",bodyParser.urlencoded({extended:false}),(req,res)=>{
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin': '*'
+  })
+  console.log(req.body);
+  res.end(JSON.stringify({
+    code:1,
+    body:req.body
+  }))
 })
 
 
